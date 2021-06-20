fca3c5a4c96af4dbbadeda8e366615d2605db4a1
Jesus Pereira <jesusbotw@gmail.com>
2021-06-19 20:03:47 -0400

    Add new modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
-#[cfg(test)]
-mod tests {
-  #[test]
-  fn it_works() {
-    assert_eq!(2 + 2, 4);
-  }
-}
+//! # Tinify Crate
+//!
+//! `tinify-rs` is a Rust client for the Tinify API.
+//! Used for TinyPNG and TinyJPG. Tinify compresses your images intelligently. 
+//! 
+//! Read more at https://tinify.com
+// --snip--
+
+mod client;
+mod source;
+mod result;
+mod tmp_file;
+mod error;
+pub mod tinify;
